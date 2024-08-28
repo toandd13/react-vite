387bf03ee6b8ca7857dd32853bd1270a5a366869
toandd13 <toanddph33737@fpt.edu.vn>
2024-08-28 16:16:07 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,15 @@
 //() => {}
 // function App() {
 //component = html + css + js
+//JSX
+//Fragment
+import './style.css'
 const MyComponent = () => {
     return (
-        <div>Toàn & ToànPro update</div>
+        <>
+            <div>Toàn & ToànPro update</div>
+            <div className="child">2004</div>
+        </>
     );
 }
 export default MyComponent
