6e9a67aee8a147cb85ba52a47d9890c81cdf93f4
toandd13 <toanddph33737@fpt.edu.vn>
2024-08-28 16:37:59 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -5,9 +5,19 @@
 //Fragment
 import './style.css'
 const MyComponent = () => {
+    // const name = "Toàn" //String 
+    // const name = 20  //Number
+    // const name = true //boolean
+    // const name = undefined
+    // const name = null
+    const name = [1, 2, 3]
+    // const name = {
+    //     name: "Toàn",
+    //     age: 20
+    // }
     return (
         <>
-            <div>Toàn & ToànPro update</div>
+            <div>{JSON.stringify(name)} & ToànPro update</div>
             <div className="child">2004</div>
         </>
     );
