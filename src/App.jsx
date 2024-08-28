f7b00a4eed9963a920f302880d0a85c9a6ebe0fd
toandd13 <toanddph33737@fpt.edu.vn>
2024-08-28 15:51:23 +0700

    video 29

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-//() => {}
-// function App() {
-//component = html + css + js
-const MyComponent = () => {
-  return (
-    <div>Toàn & ToànPro update</div>
-  );
-}
+import MyComponent from './components/learn/MyComponent'
+import { SecondComponent, ThirdComponent } from './components/learn/SecondComponent'
 
 
 const App = () => {
@@ -27,6 +21,8 @@ const App = () => {
       </div>
       <h1>Hello World Toàn Pro</h1>
       <MyComponent />
+      <SecondComponent />
+      <ThirdComponent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,9 @@
+//() => {}
+// function App() {
+//component = html + css + js
+const MyComponent = () => {
+    return (
+        <div>Toàn & ToànPro update</div>
+    );
+}
+export default MyComponent
