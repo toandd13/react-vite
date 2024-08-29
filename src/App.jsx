ea77c149f96fe92940d042c0a0025f4a47114dd4
toandd13 <toanddph33737@fpt.edu.vn>
2024-08-29 14:31:31 +0700

    video 34

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,22 @@ import TodoData from './components/todo/todoData'
 import TodoNew from './components/todo/todoNew'
 import reactLogo from './assets/react.svg'
 const App = () => {
+  const name = "David";
+  const data = {
+    name: "Toàn",
+    age: 20
+  }
+
 
 
   return (
     <div className="todo-container">
       <div className="todo-title">Todo List</div>
       <TodoNew />
-      <TodoData />
+      <TodoData
+        name={name}
+        data={data}
+      />
       <div className='todo-img'>
         <img src={reactLogo} />
       </div>
--- src/components/todo/todoData.jsx
+++ src/components/todo/todoData.jsx
@@ -1,6 +1,11 @@
-const TodoData = () => {
+const TodoData = (props) => {
+    //props là 1 biến object{}
+    // const { name, data } = props
+    const name = props.name
     return (
+
         <div className="todo-data">
+            <div>My Name is {name}</div>
             <div>Leaning React</div>
             <div>Watching Youtube</div>
         </div>
