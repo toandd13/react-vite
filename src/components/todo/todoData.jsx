da9f489ec9b15f93b4a78a91c42490c39fbf4605
toandd13 <toanddph33737@fpt.edu.vn>
2024-08-29 11:25:29 +0700

    video 33

---
+++ src/components/todo/todoData.jsx
@@ -0,0 +1,9 @@
+const TodoData = () => {
+    return (
+        <div className="todo-data">
+            <div>Leaning React</div>
+            <div>Watching Youtube</div>
+        </div>
+    )
+}
+export default TodoData
