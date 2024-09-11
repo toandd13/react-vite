c46e25688789452f0dc85abda9eb1100259442f6
toandd13 <toanddph33737@fpt.edu.vn>
2024-09-11 16:28:51 +0700

    video 67

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,27 +1,45 @@
 import { Button, Input } from "antd"
+import { useState } from "react"
 
 const UserForm = () => {
+    const [fullName, setFullName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
+    const handleClick = () => {
+        console.log("check", { fullName, email, password, phoneNumber })
+    }
     return (
         <div className="user-form" style={{ margin: "20px" }}>
             <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
                 <div>
                     <span>Full Name</span>
-                    <Input />
+                    <Input
+                        onChange={(event) => { setFullName(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Email</span>
-                    <Input />
+                    <Input
+                        onChange={(event) => { setEmail(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Password</span>
-                    <Input.Password />
+                    <Input.Password
+                        onChange={(event) => { setPassword(event.target.value) }}
+                    />
                 </div>
                 <div>
                     <span>Phone Number</span>
-                    <Input />
+                    <Input
+                        onChange={(event) => { setPhoneNumber(event.target.value) }}
+                    />
                 </div>
                 <div>
-                    <Button type="primary">Create User</Button>
+                    <Button
+                        onClick={handleClick}
+                        type="primary">Create User</Button>
                 </div>
             </div>
         </div>
