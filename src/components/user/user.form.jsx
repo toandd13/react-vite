d0430ff424b9d082becc48a7b06e513dbf180321
toandd13 <toanddph33737@fpt.edu.vn>
2024-09-18 14:03:18 +0700

    video 76

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,4 +1,4 @@
-import { Button, Input, notification } from "antd"
+import { Button, Flex, Input, notification, Modal } from "antd"
 import { useState } from "react"
 import { createUserApi } from "../../service/api.service";
 
@@ -7,14 +7,15 @@ const UserForm = () => {
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [phone, setPhone] = useState("")
-
-    const handleClick = async () => {
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const handleSubmit = async () => {
         const res = await createUserApi(fullName, email, password, phone)
         if (res.data) {
             notification.success({
                 message: "create user",
                 description: "Tạo mới user thành công"
             })
+            setIsModalOpen(false)
         } else {
             notification.error({
                 message: "error create user",
@@ -27,39 +28,55 @@ const UserForm = () => {
 
 
     return (
-        <div className="user-form" style={{ margin: "20px" }}>
-            <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
-                <div>
-                    <span>Full Name</span>
-                    <Input
-                        onChange={(event) => { setFullName(event.target.value) }}
-                    />
-                </div>
-                <div>
-                    <span>Email</span>
-                    <Input
-                        onChange={(event) => { setEmail(event.target.value) }}
-                    />
-                </div>
-                <div>
-                    <span>Password</span>
-                    <Input.Password
-                        onChange={(event) => { setPassword(event.target.value) }}
-                    />
-                </div>
-                <div>
-                    <span>Phone Number</span>
-                    <Input
-                        onChange={(event) => { setPhone(event.target.value) }}
-                    />
-                </div>
-                <div>
-                    <Button
-                        onClick={handleClick}
-                        type="primary">Create User</Button>
-                </div>
+
+        <div className="user-form" style={{ margin: "10px" }}>
+            <div style={{ display: "Flex", justifyContent: "space-between" }}>
+                <h3>Table User</h3>
+                <Button
+                    onClick={() => setIsModalOpen(true)}
+                    type="primary">Create User</Button>
             </div>
+            <Modal
+                title="CREATE USER"
+                open={isModalOpen}
+                onOk={() => handleSubmit()}
+                onCancel={() => setIsModalOpen(false)}
+                maskClosable={false}
+                okText="CREATE"
+            >
+
+                <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
+                    <div>
+                        <span>Full Name</span>
+                        <Input
+                            onChange={(event) => { setFullName(event.target.value) }}
+                        />
+                    </div>
+                    <div>
+                        <span>Email</span>
+                        <Input
+                            onChange={(event) => { setEmail(event.target.value) }}
+                        />
+                    </div>
+                    <div>
+                        <span>Password</span>
+                        <Input.Password
+                            onChange={(event) => { setPassword(event.target.value) }}
+                        />
+                    </div>
+                    <div>
+                        <span>Phone Number</span>
+                        <Input
+                            onChange={(event) => { setPhone(event.target.value) }}
+                        />
+                    </div>
+
+                </div>
+            </Modal>
         </div>
+
+
+
 
     )
 }
