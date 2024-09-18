6de56c53a65047bee71ee94a074cd13c7bacc303
toandd13 <toanddph33737@fpt.edu.vn>
2024-09-18 11:31:43 +0700

    video 73

--- src/components/user/user.form.jsx
+++ src/components/user/user.form.jsx
@@ -1,22 +1,31 @@
-import { Button, Input } from "antd"
-import axios from "axios"
+import { Button, Input, notification } from "antd"
 import { useState } from "react"
+import { createUserApi } from "../../service/api.service";
 
 const UserForm = () => {
     const [fullName, setFullName] = useState("")
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
     const [phone, setPhone] = useState("")
-    const handleClick = () => {
-        const URL_BACKEND = "http://localhost:8080/api/v1/user";
-        const data = {
-            fullName: fullName,
-            email: email,
-            password: password,
-            phone: phone,
+
+    const handleClick = async () => {
+        const res = await createUserApi(fullName, email, password, phone)
+        if (res.data) {
+            notification.success({
+                message: "create user",
+                description: "Tạo mới user thành công"
+            })
+        } else {
+            notification.error({
+                message: "error create user",
+                description: JSON.stringify(res.message)
+            })
         }
-        axios.post(URL_BACKEND, data)
+
     }
+
+
+
     return (
         <div className="user-form" style={{ margin: "20px" }}>
             <div style={{ display: "flex", gap: "15px", flexDirection: "column" }}>
